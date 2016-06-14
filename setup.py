184a57c914a4fabdb01bbdc701af5c4a3d429410
Jonathan Virga <jonathan.virga@gmail.com>
2016-06-14 08:04:21 +0200

    Fix version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='papillon',
-      version='1.0',
+      version='1.1',
       description='papillon',
       author='alabintheair',
       author_email='jonathan.virga@alabintheair.cc',
