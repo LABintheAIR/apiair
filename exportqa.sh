e7332f8d5741bed126daf15d72f6e9f3e2f78311
Jonathan Virga <jonathan.virga@labintheair.cc>
2016-09-02 07:42:33 +0200

    Fix listening port.

--- exportqa.sh
+++ exportqa.sh
@@ -9,5 +9,5 @@ log=exportqa.log
 echo "--- $( date ) ---" >>${log}
 python3.5 exportqa.py http://papillon-jnth.rhcloud.com 1>>${log} 2>>${log}
 echo >>${log}
-python3.5 exportqa.py http://j6tron.labintheair.cc 1>>${log} 2>>${log}
+python3.5 exportqa.py http://j6tron.labintheair.cc:16500 1>>${log} 2>>${log}
 echo >>${log}
